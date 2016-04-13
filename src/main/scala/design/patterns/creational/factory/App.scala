913d92841f3b4c4c35dbf59e491b9b43a97c4d47
noprom <tyee.noprom@qq.com>
2016-04-14 06:36:38 +0800

    implement a new simple App to illustrate the Factory design pattern

---
+++ src/main/scala/design/patterns/creational/factory/App.scala
@@ -0,0 +1,18 @@
+package design.patterns.creational.factory
+
+/**
+  * App
+  * 工厂模式演示App
+  *
+  * Author: Noprom <tyee.noprom@qq.com>
+  * Date: 4/14/16 6:34 AM.
+  */
+object App {
+
+  def main(args: Array[String]) {
+    val clientMySql: DatabaseClient = new MysqlClient
+    val clientPgSql: DatabaseClient = new PgSqlClient
+    clientMySql.executeQuery("SELECT * FROM users")
+    clientPgSql.executeQuery("SELECT * FROM employees")
+  }
+}
