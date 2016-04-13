cf8bb230c3260b07e282fe7662e20f3f298fa5db
noprom <tyee.noprom@qq.com>
2016-04-14 06:34:13 +0800

    add DBClienct

---
+++ src/main/scala/design/patterns/creational/factory/DatabaseClient.scala
@@ -0,0 +1,26 @@
+package design.patterns.creational.factory
+
+/**
+  * 工厂模式的客户端
+  *
+  * Author: Noprom <tyee.noprom@qq.com>
+  * Date: 4/14/16 6:31 AM.
+  */
+abstract class DatabaseClient {
+  def executeQuery(query: String): Unit = {
+    val connection = connect()
+    connection.executeQuery(query)
+  }
+
+  protected def connect(): SimpleConnection
+}
+
+class MysqlClient extends DatabaseClient {
+  override protected def connect(): SimpleConnection = new
+      SimpleMysqlConnection
+}
+
+class PgSqlClient extends DatabaseClient {
+  override protected def connect(): SimpleConnection = new
+      SimplePgSqlConnection
+}
--- src/main/scala/design/patterns/creational/factory/SimpleConnection.scala
+++ src/main/scala/design/patterns/creational/factory/SimpleConnection.scala
@@ -1,6 +1,7 @@
 package design.patterns.creational.factory
 
 /**
+  * 工厂模式
   *
   * Author: Noprom <tyee.noprom@qq.com>
   * Date: 4/14/16 6:25 AM.
@@ -16,5 +17,13 @@ class SimpleMysqlConnection extends SimpleConnection {
 
   override def executeQuery(query: String): Unit = {
     System.out.println(s"Executing the query '$query' the MySQL way.")
+  }
+}
+
+class SimplePgSqlConnection extends SimpleConnection {
+  override def getName(): String = "SimplePgSqlConnection"
+
+  override def executeQuery(query: String): Unit = {
+    System.out.println(s"Executing the query '$query' the PgSQL way.")
   }
 }
