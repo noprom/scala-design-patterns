3d369ec17b360f19d0b350299318eedd6d49b978
noprom <tyee.noprom@qq.com>
2016-04-15 06:13:35 +0800

    add another connection printer

---
+++ src/main/scala/design/patterns/creational/factory/SimpleConnectionPrinter.scala
@@ -0,0 +1,11 @@
+package design.patterns.creational.factory
+
+/**
+  * SimpleConnectionPrinter
+  *
+  * Author: Noprom <tyee.noprom@qq.com>
+  * Date: 4/15/16 6:12 AM.
+  */
+trait SimpleConnectionPrinter {
+  def printSimpleConnection(connection: SimpleConnection): Unit
+}
