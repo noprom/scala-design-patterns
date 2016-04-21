8c3546651996e53990ef19f24f2ab5f3113a8ccd
noprom <tyee.noprom@qq.com>
2016-04-22 06:19:37 +0800

    add AbstractFactoryApp

--- src/main/scala/design/patterns/creational/factory/abstractfac/DatabaseConnectorFactory.scala
+++ src/main/scala/design/patterns/creational/factory/abstractfac/DatabaseConnectorFactory.scala
@@ -20,3 +20,19 @@ class MySqlFactory extends DatabaseConnectorFactory {
 class PgSqlFactory extends DatabaseConnectorFactory {
   override def connect(): SimpleConnection = new SimplePgSqlConnection
 }
+
+class DatabaseClient(connectorFactory: DatabaseConnectorFactory) {
+  def executeQuery(query: String): Unit = {
+    val connection = connectorFactory.connect()
+    connection.executeQuery(query)
+  }
+}
+
+object AbstractFactoryApp {
+  def main(args: Array[String]): Unit = {
+    val clientMySql: DatabaseClient = new DatabaseClient(new MySqlFactory)
+    val clientPgSql: DatabaseClient = new DatabaseClient(new PgSqlFactory)
+    clientMySql.executeQuery("SELECT * FROM users")
+    clientPgSql.executeQuery("SELECT * FROM employees")
+  }
+}
